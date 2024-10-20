a55d0ccfa53f74d23a8f215db41fb3d207068298
Korvo-iam <emilaubakirov@gmail.com>
2024-10-20 20:06:38 +0500

    2006201024

--- gendiff/gendiff_fun.py
+++ gendiff/gendiff_fun.py
@@ -9,16 +9,16 @@ def generate_diff(first, second):
     stroka = '{'
     file1 = json_inside1
     file2 = json_inside2
-    first_list = list(file1.keys())
-    second_list = list(file2.keys())
+    first_list_keys = list(file1.keys())
+    second_list_keys = list(file2.keys())
     gen_list = list(set(list(file1.keys()) + list(file2.keys())))
     gen_list.sort()
     for el in gen_list:
-        if el not in second_list:
+        if el not in second_list_keys:
             if type(file1[el]) is bool:
                 file1[el] = str(file1[el]).lower()
             stroka = stroka + '\n' + f'  - {str(el)}: {file1[el]}'
-        elif el not in first_list:
+        elif el not in first_list_keys:
             if type(file1[el]) is bool:
                 file1[el] = str(file1[el]).lower()
             stroka = stroka + '\n' + f'  + {str(el)}: {file2[el]}'
