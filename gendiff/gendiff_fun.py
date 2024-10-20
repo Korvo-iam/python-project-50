ff0446e8255d38bfaa6d0c98940e4a9c31eaa055
Korvo-iam <emilaubakirov@gmail.com>
2024-10-20 20:00:33 +0500

    2000201024

--- gendiff/gendiff_fun.py
+++ gendiff/gendiff_fun.py
@@ -14,16 +14,21 @@ def generate_diff(first, second):
     gen_list = list(set(list(file1.keys()) + list(file2.keys())))
     gen_list.sort()
     for el in gen_list:
-        if type(file1[el]) is bool:
-            file1[el] = str(file1[el])
-            print(file1[el])
         if el not in second_list:
+            if type(file1[el]) is bool:
+                file1[el] = str(file1[el]).lower()
             stroka = stroka + '\n' + f'  - {str(el)}: {file1[el]}'
         elif el not in first_list:
+            if type(file1[el]) is bool:
+                file1[el] = str(file1[el]).lower()
             stroka = stroka + '\n' + f'  + {str(el)}: {file2[el]}'
         elif file1[el] == file2[el]:
+            if type(file1[el]) is bool:
+                file1[el] = str(file1[el]).lower()
             stroka = stroka + '\n' + f'    {str(el)}: {file1[el]}'
         elif file1[el] != file2[el]:
+            if type(file1[el]) is bool:
+                file1[el] = str(file1[el]).lower()
             stroka = stroka + '\n' + f'  - {str(el)}: {file1[el]}'
             stroka = stroka + '\n' + f'  + {str(el)}: {file2[el]}'
     stroka = stroka + '\n' + '}'
