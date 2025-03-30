c62fb5350469c41a0a4d39e517b4ecbd97de8e11
Korvo-iam <emilaubakirov@gmail.com>
2025-03-31 02:56:58 +0500

    025631032025

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -7,6 +7,8 @@ file3 = 'files/file3.json'
 file4 = 'files/file4.json'
 file5 = 'files/file5.yaml'
 file6 = 'files/file6.yml'
+file7 = 'files/file7.json'
+file8 = 'files/file8.json'
 
 
 def test_generate_diff_1():
@@ -72,5 +74,54 @@ def test_generate_diff_6():
   - host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
+}'''
+    assert difference == expected
+
+
+def test_generate_diff_7():
+    difference = gendiff_fun.generate_diff(file7, file8)
+    expected = '''{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }'''
     assert difference == expected
