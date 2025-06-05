05da0e2b0a60a6e52ca4bee4c425a91f7c6f9146
Korvo-iam <emilaubakirov@gmail.com>
2025-06-05 21:19:14 +0500

    211905052025

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -30,10 +30,7 @@ def generate_diff(first, second, format_name='stylish'):
         file2 = second
     else:
         file2 = open_file(second)
-    print(file1)
-    print(file2)
     diff = set_status(file1, file2)
-    print(diff)
     diff_inside = diff['root']
     fin_string = ''
     if format_name == 'plain':
